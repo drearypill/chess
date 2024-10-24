7dfb4f9cdeff61a9135841d8e40f96bc0cf8e34b
drearypill <lilystyle100@gmail.com>
2024-10-23 22:57:32 -0600

    set up structure

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -4,11 +4,4 @@ import model.AuthData;
 
 public interface AuthDAO {
 
-    void addAuth(AuthData authData);
-
-    void deleteAuth(String authToken);
-
-    AuthData getAuth(String authToken) throws DataAccessException;
-
-    void clear();
 }
