48367cdd74506ec60ebc136e5a9056d3c7d8e30f
drearypill <lilystyle100@gmail.com>
2024-11-06 16:34:26 -0700

    header aligns | blocks are dark and light

--- client/src/main/java/ui/ChessBoardUI.java
+++ client/src/main/java/ui/ChessBoardUI.java
@@ -3,6 +3,7 @@ package ui;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 import chess.ChessBoard;
+import chess.ChessGame;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
@@ -17,7 +18,7 @@ public class ChessBoardUI {
         setBackground(out);
 
         drawTopLetters(out);
-        drawRow(out);
+        drawRow(out, 1);
 
         out.print(SET_BG_COLOR_BLACK);
         out.print(SET_TEXT_COLOR_WHITE);
@@ -29,48 +30,33 @@ public class ChessBoardUI {
         out.print(EMPTY);
 
         for (int boardCol = 0; boardCol < 8; ++boardCol) {
+            out.print(" ");
             printTopLetters(out, letters[boardCol]);
-            out.print(EMPTY);
+            out.print(" ");
 
         }
 
         out.println();
     }
 
-    private static void drawRow(PrintStream out) {
+    private static void drawRow(PrintStream out, int boardRow) {
         ChessPiece[][] board = new ChessPiece[8][8];
         ChessBoard chessBoard = new ChessBoard();
         chessBoard.resetBoard();
         chessBoard.getBoard();
 //        out.print(chessBoard);
-        String[] row1 = { WHITE_ROOK, WHITE_KNIGHT, WHITE_BISHOP, WHITE_QUEEN, WHITE_KING, WHITE_KNIGHT, WHITE_BISHOP, WHITE_KNIGHT};
         out.print(EMPTY);
         setDarkSquare(out);
         for (int boardCol = 1; boardCol < 9; ++boardCol) {
-            if (chessBoard.getPiece(new ChessPosition(1, boardCol)) != null) {
-//                printTopLetters(out, board[1][boardCol].toString());
-                printTopLetters(out, chessBoard.getPiece(new ChessPosition(1, boardCol)).toString());
-//                out.println();
-//                printTopLetters(out, chessBoard.getPiece(new ChessPosition(2, boardCol)).toString());
-
-                out.print(EMPTY);
+            if ((boardCol + boardRow) % 2 == 0) {
+                setDarkSquare(out);
             }
-            else {out.print(EMPTY);}
-
+            else {
+                setLightSquare(out);
+            }
+            printPiece(out, chessBoard.getPiece(new ChessPosition(boardRow, boardCol)));
         }
         out.println();
-
-
-        for (int boardCol = 1; boardCol < 9; ++boardCol) {
-            if (chessBoard.getPiece(new ChessPosition(1, boardCol)) != null) {
-//                printTopLetters(out, board[1][boardCol].toString());
-                printTopLetters(out, chessBoard.getPiece(new ChessPosition(2, boardCol)).toString());
-
-                out.print(EMPTY);
-            }
-            else {out.print(EMPTY);}
-
-        }
     }
 
 
@@ -89,6 +75,35 @@ public class ChessBoardUI {
     }
     private static void setDarkSquare(PrintStream out) {
         out.print(SET_BG_COLOR_DARK_GREY);
+    }
+
+    private static void printPiece(PrintStream out, ChessPiece piece) {
+        if (piece != null) {
+            if (piece.getTeamColor() == ChessGame.TeamColor.WHITE) {
+                out.print(" ");
+
+                if (piece.getPieceType() == ChessPiece.PieceType.ROOK) {
+                    out.print(WHITE_ROOK);
+                }
+                else if (piece.getPieceType() == ChessPiece.PieceType.KNIGHT) {
+                    out.print(WHITE_KNIGHT);
+                }
+                else if (piece.getPieceType() == ChessPiece.PieceType.BISHOP) {
+                    out.print(WHITE_BISHOP);
+                }
+                else if (piece.getPieceType() == ChessPiece.PieceType.QUEEN) {
+                    out.print(WHITE_QUEEN);
+                }
+                else if (piece.getPieceType() == ChessPiece.PieceType.KING) {
+                    out.print(WHITE_KING);
+                }
+
+                out.print(" ");
+
+            }
+
+        }
+        else {out.print(EMPTY);}
     }
 
     private String[][] makeStartBoard() {
