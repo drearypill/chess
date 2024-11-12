c767876b85fd627f33ea08abe78a8c6b0f9a2e7a
drearypill <lilystyle100@gmail.com>
2024-11-12 14:13:43 -0700

    ServerFacade

--- client/src/main/java/ServerFacade.java
+++ client/src/main/java/ServerFacade.java
@@ -4,16 +4,68 @@ import exception.ResponseException;
 
 import java.io.*;
 import java.net.*;
+import java.util.Map;
+import java.util.Collections;
 
 public class ServerFacade {
 
     private final String serverUrl;
+    String authToken;
 
     public ServerFacade(String url) {
         serverUrl = url;
     }
 
-    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws ResponseException {
+    public boolean register(String username, String password, String email) {
+        var body = Map.of("username", username, "password", password, "email", email);
+        var jsonBody = new Gson().toJson(body);
+        Map resp = makeRequest("POST", "/user", jsonBody);
+        if (resp.containsKey("Error")) {
+            return false;
+        }
+        authToken = (String) resp.get("authToken");
+        return true;
+    }
+
+    /**
+     *
+     * @param username
+     * @param password
+     * @return success
+     */
+    public boolean login(String username, String password) {
+        var body = Map.of("username", username, "password", password);
+        var jsonBody = new Gson().toJson(body);
+        try {
+            Map resp = makeRequest("POST", "/session", jsonBody);
+            if (resp.containsKey("Error")) {
+                return false;
+            }
+            authToken = (String) resp.get("authToken");
+            return true;
+        } catch (ResponseException ex) {
+            return false;
+        }
+    }
+
+    public boolean logout() {
+        Map resp = makeRequest("DELETE", "/session");
+        if (resp.containsKey("Error")) {
+            return false;
+        }
+        authToken = null;
+        return true;
+    }
+
+    private Map makeRequest(String method, String endpoint) {
+        try {
+            return makeRequest(method, endpoint, null);
+        } catch (ResponseException ex) {
+            return Collections.emptyMap();
+        }
+    }
+
+    private <T> T makeRequest(String method, String path, Object request,) throws ResponseException {
         try {
             URL url = (new URI(serverUrl + path)).toURL();
             HttpURLConnection http = (HttpURLConnection) url.openConnection();
@@ -50,7 +102,7 @@ public class ServerFacade {
     private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
         T response = null;
         if (http.getContentLength() < 0) {
-            try (InputStream respBody = http.getInputStream()) {
+            try (InputStream erespBody = http.getInputStream()) {
                 InputStreamReader reader = new InputStreamReader(respBody);
                 if (responseClass != null) {
                     response = new Gson().fromJson(reader, responseClass);
