27643ee032070cb8b7f496b19a713e71f6cae25f
drearypill <lilystyle100@gmail.com>
2024-11-12 16:30:11 -0700

    client menus pre login

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,15 @@
 import chess.*;
+import ui.ClientUI;
+import client.ServerFacade;
+
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+        ServerFacade server = new ServerFacade();
+
+        ClientUI prelogin = new ClientUI(server);
+        prelogin.run();
     }
 }
